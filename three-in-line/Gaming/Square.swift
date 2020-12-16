142e60885240fbe2633aa0356a9ac66ea2cee57b
jMac <j_eberholst@gmail.com>
2020-12-16 23:09:11 +0100

    Added setChecked function, added Enum class for squareVal (Should determine if square is X, O or Empty), checked should determine if currently checked, finalized should check if checked square no longer is changeable

--- three-in-line/Gaming/Square.swift
+++ three-in-line/Gaming/Square.swift
@@ -10,9 +10,33 @@ import Foundation
 struct Square {
     var index: Int
     var checked: Bool
+    var finalized: Bool
+    var squareVal: SquareVal = .EMPTY
     
     func isValid() -> Bool {
-        print(checked)
-        return checked
+        print(finalized)
+        return finalized
     }
+    
+    mutating func setChecked(){
+       
+        guard !finalized else { return }
+        guard squareVal == .EMPTY else { return }
+        
+        print("Finalized: \(finalized)")
+        print("Checked: \(checked)")
+        print("SquareVal: \(squareVal)")
+        
+        if checked {
+            checked = false
+        } else {
+            checked = true
+        }
+    }
+}
+
+enum SquareVal: String {
+    case X = "X"
+    case O = "O"
+    case EMPTY = ""
 }
