6457f687b318107dc3f63542895a167573c8563b
jMac <j_eberholst@gmail.com>
2021-01-12 11:46:06 +0100

    Minor fix causing unknown crash, fixed itself somehow

--- three-in-line/ViewControllers/GameModeViewController.swift
+++ three-in-line/ViewControllers/GameModeViewController.swift
@@ -10,15 +10,15 @@ import UIKit
 class GameModeViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
     @IBOutlet weak var gameModePicker: UIPickerView!
-    	
+
     private let userDefaultsSelectedColumn = "gameModeSelectedColumns"
     let gameModes = GameModes()
     var selectedGameModeColumns: Int = 3
-    
+
     @IBAction func btnConfirm(_ sender: UIButton) {
         savedSelectedRow(columnCount: selectedGameModeColumns)
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,26 +26,27 @@ class GameModeViewController: UIViewController, UIPickerViewDataSource, UIPicker
         gameModePicker.delegate = self
 
     }
-    
+
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return gameModes.getListCount()
+        return gameModes.getListCount()	
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return "\(gameModes.getList()[row].name)"
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         selectedGameModeColumns = gameModes.getList()[row].columns
     }
-   
+
     func savedSelectedRow(columnCount: Int) {
         let defaults = UserDefaults.standard
-        defaults.set(columnCount, forKey: userDefaultsSelectedColumn)
+        defaults.setValue(columnCount, forKey: "gameModeSelectedColumns")
+       // defaults.set(columnCount, forKey: userDefaultsSelectedColumn)
         defaults.synchronize()
     }
 
